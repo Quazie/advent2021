96b0f8164362279cce8dfb9274833339c9e9e318
Quazie Kwaselow <quazie@gmail.com>
2021-12-02 19:32:02 -0800

    Prepping for day 3, and added a little of 2015

---
+++ 2015/day 1/code.py
@@ -0,0 +1,44 @@
+DEBUG = True
+
+def dprint(*args):
+    if DEBUG:
+        print("".join(map(str,args)))
+
+filename ="input1.txt"
+if DEBUG:
+    filename = "test1.txt"
+
+with open(filename) as f:
+    file_lines = f.readlines()
+
+processed_line = file_lines[0].strip()
+
+def part1(line):
+    floor = 0
+    for char in line:
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+        else:
+            print("What is " + char)
+    print("Floor: " + str(floor))
+
+def part2(line):
+    floor = 0
+    for i,char in enumerate(line):
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+        else:
+            print("What is " + char)
+        
+        if floor < 0:
+            print("basement at: " + str(i+1))
+            break
+    
+
+
+part1(processed_line)
+part2(processed_line)
---
+++ 2015/day2/code.py
@@ -0,0 +1,39 @@
+DEBUG = False
+
+def dprint(*args):
+    if DEBUG:
+        print("".join(map(str,args)))
+
+filename ="input1.txt"
+if DEBUG:
+    filename = "test1.txt"
+
+with open(filename) as f:
+    file_lines = f.readlines()
+
+def process_line(line):
+    return list(map(int,line.split('x')))
+
+processed_lines = list(map(process_line, file_lines))
+
+def part1(lines):
+    amount = 0
+    for line in lines:
+        l,w,h = line
+        amount += (2*l*w + 2*w*h + 2*h*l + min(l*w, min(l*h, h*w)))
+
+    print("Total: " + str(amount))
+
+def part2(lines):
+    amount = 0
+    for line in lines:
+        l,w,h = line
+        min_perim = min(2*l+2*h, min(2*l+2*w, 2*h+2*w))
+        bow = l * w * h
+        amount += min_perim + bow
+        dprint("bow: " + str(bow) + " perim: " + str(min_perim
+        ))
+    print("Total: " + str(amount))
+
+part1(processed_lines)
+part2(processed_lines)
