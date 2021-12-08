831af370b401aa738b9d1c9a3797a8001c571c4c
Quazie Kwaselow <quazie@gmail.com>
2021-12-07 17:52:43 -0800

    Day 7, and 8 templated

--- day6/code.py
+++ day6/code.py
@@ -1,4 +1,4 @@
-DEBUG = False
+DEBUG = True
 
 def dprint(*args):
     if DEBUG:
@@ -11,22 +11,23 @@ if DEBUG:
 with open(filename) as f:
     file_lines = f.readlines()
 
-
-
 processed_lines = list(map(int, file_lines[0].split(',')))
 
+BREED_EVERY = 7
+INITIAL_BREED_DELAY = 2
+
 def calculate_fish(days, starting_fish):
-    days_til_breed = [0] * 9
+    days_til_breed = [0] * (BREED_EVERY+INITIAL_BREED_DELAY)
 
     dprint(starting_fish)
     for fish in starting_fish:
         dprint(fish)
         days_til_breed[fish] += 1
     dprint(days_til_breed)
-    for i in range(days):
+    for _ in range(days):
         new_fish = days_til_breed.pop(0)
         days_til_breed.append(new_fish)
-        days_til_breed[6] += new_fish
+        days_til_breed[BREED_EVERY-1] += new_fish # index is -1 cuz 0 indexing
         dprint(days_til_breed)
     
     print("In " + str(days) + " days - Fishes: " + str(sum(days_til_breed)))
