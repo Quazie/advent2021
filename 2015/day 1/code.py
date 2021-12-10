98eb2f3f9c1bafe12cf6714ef7e6e7270ce19806
Quazie Kwaselow <quazie@gmail.com>
2021-12-09 22:25:14 -0800

    Added black

--- 2015/day 1/code.py
+++ 2015/day 1/code.py
@@ -1,10 +1,12 @@
 DEBUG = True
 
+
 def dprint(*args):
     if DEBUG:
-        print("".join(map(str,args)))
+        print("".join(map(str, args)))
 
-filename ="input1.txt"
+
+filename = "input1.txt"
 if DEBUG:
     filename = "test1.txt"
 
@@ -12,6 +14,7 @@ with open(filename) as f:
     file_lines = f.readlines()
 
 processed_line = file_lines[0].strip()
+
 
 def part1(line):
     floor = 0
@@ -24,20 +27,20 @@ def part1(line):
             print("What is " + char)
     print("Floor: " + str(floor))
 
+
 def part2(line):
     floor = 0
-    for i,char in enumerate(line):
+    for i, char in enumerate(line):
         if char == "(":
             floor += 1
         elif char == ")":
             floor -= 1
         else:
             print("What is " + char)
-        
+
         if floor < 0:
-            print("basement at: " + str(i+1))
+            print("basement at: " + str(i + 1))
             break
-    
 
 
 part1(processed_line)
